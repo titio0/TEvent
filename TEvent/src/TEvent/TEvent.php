adbabb57cdeffa51b2deac6b226c914e3cecfd93
titio0 <60613215+titio0@users.noreply.github.com>
2020-04-29 13:30:19 +0900

    Add files via upload

---
+++ TEvent/src/TEvent/TEvent.php
@@ -0,0 +1,36 @@
+<?php
+//해당 플러그인 (TEvent)은 Fabrik-EULA에 의해 보호됩니다.//Fabrik-EULA : https://github.com/Flug-in-Fabrik/Fabrik-EULAnamespace TEvent;
+
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+
+class TEvent extends PluginBase implements Listener {
+     public function onEnable() {
+               $this->getServer ()->getPluginManager ()->registerEvents ( $this, $this );
+               $this->getLogger ()->alert("티오 이벤트 플러그인 적용완료");   }
+
+     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+       $command = $command->getName ();
+       $name = $sender->getName ();
+       $tag = "§3§l[ §f공지 §3] §f";
+
+       if ($command == "이벤트공지") {
+         if (! $sender->isOp ()) {
+           $sender->sendMessage ( $tag . "당신은 권한이 없습니다. " );
+           return true;
+                }
+                elseif (isset($args[0]) {
+                  foreach ($this->getServer()->getOnlinePlayers() as $player) {
+                     $player->addTitle("§6§l[§f!§6]§r§f " . $args[0]);}
+                }
+                else {
+                  $sender->sendMessage ( $tag . "/이벤트공지 [할말]" );
+                }
+              }
+            }
+          }
